0fe449096b0adf85d712490c255b325140440afb
Francesco Piscani <stem.apks@gmail.com>
2016-12-29 15:00:31 -0500

    Add files via upload

---
+++ tut1_dispImage.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp> //Include file for every supported OpenCV function
+using namespace cv;
+	
+int main( int argc, char** argv ) {
+
+	cv::Mat img = cv::imread(argv[1],-1);
+	if( img.empty() ) return -1;
+	cv::namedWindow( "Example1", cv::WINDOW_AUTOSIZE );
+	cv::imshow( "Example1", img );
+	cv::waitKey( 0 );
+	cv::destroyWindow( "Example1" );
+	return 0;
+}
